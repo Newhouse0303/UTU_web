f7b6328bf431fe197698c3494e830f2105d386c1
Saara <saara.newhouse@icloud.com>
2024-09-18 14:54:54 +0300

    2.8 finished

--- src/App_2.6.jsx
+++ src/App_2.6.jsx
@@ -4,48 +4,27 @@ const Numbers = ({ persons }) => {
   return (
     <div>
       {persons.map((person) => (
-        <p key={new Date()}>
-          {person.name} {person.number}
-        </p>
+        <p key={person.name}>{person.name}</p>
       ))}
     </div>
   );
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: 12345678 },
-  ]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
 
-  const handleNameChange = (e) => {
+  const handleInputChange = (e) => {
     setNewName(e.target.value);
-  };
-
-  const handleNumberChange = (e) => {
-    setNewNumber(e.target.value);
   };
 
   const addPerson = (e) => {
     e.preventDefault();
-
-    if (persons.some((person) => person.name === newName)) {
-      alert(`${newName} is already added to the phonebook`);
-      setNewName("");
-      return;
-    }
-
-    if (newName === "") return;
-
     const personObject = {
       name: newName,
-      number: newNumber,
     };
-
     setPersons(persons.concat(personObject));
     setNewName("");
-    setNewNumber("");
   };
 
   return (
@@ -53,8 +32,7 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
-          number: <input value={newNumber} onChange={handleNumberChange} />
+          name: <input value={newName} onChange={handleInputChange} />
         </div>
         <div>
           <button type="submit">add</button>
--- src/App_2.7.jsx
+++ src/App_2.7.jsx
@@ -4,27 +4,18 @@ const Numbers = ({ persons }) => {
   return (
     <div>
       {persons.map((person) => (
-        <p key={new Date()}>
-          {person.name} {person.number}
-        </p>
+        <p key={person.name}>{person.name}</p>
       ))}
     </div>
   );
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: 12345678 },
-  ]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
 
-  const handleNameChange = (e) => {
+  const handleInputChange = (e) => {
     setNewName(e.target.value);
-  };
-
-  const handleNumberChange = (e) => {
-    setNewNumber(e.target.value);
   };
 
   const addPerson = (e) => {
@@ -40,12 +31,10 @@ const App = () => {
 
     const personObject = {
       name: newName,
-      number: newNumber,
     };
 
     setPersons(persons.concat(personObject));
     setNewName("");
-    setNewNumber("");
   };
 
   return (
@@ -53,8 +42,7 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
-          number: <input value={newNumber} onChange={handleNumberChange} />
+          name: <input value={newName} onChange={handleInputChange} />
         </div>
         <div>
           <button type="submit">add</button>
