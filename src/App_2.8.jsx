de5fdb34e0e3de178d67b159101bbd09f3691617
Saara <saara.newhouse@icloud.com>
2024-09-19 14:29:29 +0300

    axios config

---
+++ src/App_2.8.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+const Numbers = ({ persons }) => {
+  return (
+    <div>
+      {persons.map((person) => (
+        <p key={new Date()}>
+          {person.name} {person.number}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: 12345678 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
+
+  const addPerson = (e) => {
+    e.preventDefault();
+
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to the phonebook`);
+      setNewName("");
+      return;
+    }
+
+    if (newName === "") return;
+
+    const personObject = {
+      name: newName,
+      number: newNumber,
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName("");
+    setNewNumber("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Numbers persons={persons} />
+    </div>
+  );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+//import axios from "axios";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// const promise = axios.get("http://localhost:3001/persons");
+// console.log(promise);
+
+// const promise2 = axios.get("http://localhost:3001/foobar");
+// console.log(promise2);
+
+//import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(<App />);
+
+// const promise = axios.get("http://localhost:3001/persons");
+
+// promise.then((response) => {
+//   console.log(response);
+// });
+
+// axios.get("http://localhost:3001/persons").then((response) => {
+//   const notes = response.data;
+//   console.log(notes);
+// });
