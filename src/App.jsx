88b129e208faeadfd0fdd3cdb48257a552b1ee90
Saara <saara.newhouse@icloud.com>
2024-09-19 14:37:25 +0300

    axios ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 const Numbers = ({ persons }) => {
   return (
     <div>
       {persons.map((person) => (
-        <p key={new Date()}>
+        <p key={person.id}>
           {person.name} {person.number}
         </p>
       ))}
@@ -13,11 +15,18 @@ const Numbers = ({ persons }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: 12345678 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
+  console.log("render", persons.length, "persons");
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
