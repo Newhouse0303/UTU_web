db80f98f8bd9702ab574d5dd0a5f153c37db5fef
Saara <saara.newhouse@icloud.com>
2024-09-30 17:37:55 +0300

    ready

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const baseUrl_for_build = "/api/persons";
-const baseUrl_for_dev = "http://localhost:3001/persons";
-const baseUrl = baseUrl_for_dev;
+const url_for_build = "/api/persons";
+const url_for_dev = "http://localhost:3001/persons"; // with "npm run server"
+const baseUrl = url_for_dev;
 
 const getAll = () => {
   return axios.get(baseUrl);
